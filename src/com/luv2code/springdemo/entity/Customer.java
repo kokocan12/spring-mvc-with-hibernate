1337186084a155f484703893ff3b20933b381bf3
kokocan12 <yangjaewon7755@naver.com>
2020-07-29 23:17:32 +0900

    customer수정

--- src/com/luv2code/springdemo/entity/Customer.java
+++ src/com/luv2code/springdemo/entity/Customer.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Table(name="customer")
 public class Customer {
 	
-	//hello
+	//hello2
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")	
