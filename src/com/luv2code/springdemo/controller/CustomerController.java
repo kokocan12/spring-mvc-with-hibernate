80f0307116a3cfaa11cb8868c79cdd62ec57695b
kokocan12 <yangjaewon7755@naver.com>
2020-07-30 11:58:04 +0900

    컨트롤러 작성완료

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,16 +1,31 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// need to inject the customer dao
+	@Autowired
+	CustomerDAO customerDAO;
 	
 	@RequestMapping("/list")
 	public String listCustomer(Model theModel) {
+		
+		// get customers from dao
+		List<Customer> customers = customerDAO.getCustomers();
+
+		// add the customer to the model
+		theModel.addAttribute("customers", customers);
 		
 		return "list-customers";
 	}
